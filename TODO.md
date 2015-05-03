4208e7669054c494a7756a7405183a69fa2e0d5e
electricg <electric.g@gmail.com>
2015-05-03 11:28:40 +0100

    Using ffconf14 version as start

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* left-logic.png needs new colors
+* left-logic.svg needs new colors
+* new logo.png
---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+// Scroll to session
+(function() {
+  var $ = function (s) {
+    try {
+      return document.querySelectorAll(s);
+    } catch (e) {
+      return [];
+    }
+  };
+
+  var $nav = $('.nav-main');
+  var today = new Date();
+  var best = null;
+  var isConfDay = (today.getDate() === confDay.getDate()) &&
+                  (today.getMonth() === confDay.getMonth()) &&
+                  (today.getFullYear() === confDay.getFullYear());
+
+  if (isConfDay) {
+    // find the current session
+    var sessions = $('.js-session');
+    var length = sessions.length;
+    best = sessions[0];
+
+    for (var i = 0; i < length; i++) {
+      if ((Date.parse(sessions[i].getAttribute('data-date')) - (5 * 1000 * 60)) < today) {
+        best = sessions[i];
+      }
+    }
+  }
+
+  // if today is conference day, then scroll the current session in to view
+  if (isConfDay && best && !window.location.hash) {
+    setTimeout(function () {
+      best.scrollIntoView(true);
+    }, 750);
+  } else {
+    // only scroll the front page
+    // && /mobi/i.test(navigator.userAgent)
+    $nav.length > 0 && location.pathname === '/' && !location.hash && setTimeout(function () {
+      if (!pageYOffset) window.scrollTo(0, $nav[0].offsetHeight);
+    }, 750);
+  }
+})();
