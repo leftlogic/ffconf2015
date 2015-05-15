5d09d1b540972ca6908b36559fbe8cdc514ddaa6
electricg <electric.g@gmail.com>
2015-05-15 22:33:12 +0100

    Added moment.js server side

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict';
 var harp = require('harp');
 var server = require('harp-static');
+var moment = require('moment');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
 
 global.idify = function (s) { return s.replace(/&.*?;/g, '').replace(/\s+/g, '-').replace(/[^\w\-]/g, '').toLowerCase(); };
 
-harp.compile(__dirname, outputPath, function(errors){
-  if(errors) {
+// this line, although dirty, ensures that Harp templates
+// have access to moment - which given the whole partial
+// import hack doesn't work consistently across dynamic vs
+// compiled, this is the cleanest solution.
+global.moment = moment;
+
+harp.compile(__dirname, outputPath, function (errors) {
+  if (errors) {
     console.log(JSON.stringify(errors, null, 2));
     process.exit(1);
   }
