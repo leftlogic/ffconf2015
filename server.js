08045822e6d35774495bf1363e86783ed1ccdecf
Remy Sharp <remy@remysharp.com>
2016-08-31 15:59:00 +0100

    feat: make requireable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 var harp = require('harp');
-var server = require('harp-static');
+var express = require('express');
+var app = express();
 var moment = require('moment');
 var outputPath = __dirname + '/www';
 var port = process.env.PORT || 9000;
@@ -14,27 +15,33 @@ global.idify = function (s) { return s.replace(/&.*?;/g, '').replace(/\s+/g, '-'
 global.moment = moment;
 
 if (process.env.NODE_ENV !== 'production') {
-  // Yeah...don't ask.
-  var config = require('./harp.json');
-  Object.keys(config.globals).forEach(function (key) {
-    global[key] = config.globals[key];
-  });
-  // </rando>
-  var express = require('express');
-  var app = express();
-
   app.use(express.static(__dirname + '/public'));
   app.use(harp.mount(__dirname + '/public'));
 
-  app.listen(process.env.PORT || 9000);
+  if (module.parent) {
+    module.exports = app;
+  } else {
+    var server = app.listen(port, function(){
+      console.log('Listening at http://%s:%s', server.address().address, server.address().port);
+    });
+  }
 } else {
-  harp.compile(__dirname, outputPath, function (errors) {
+  app.use(express.static(__dirname + '/www', { extensions: ['html'] }));
+  if (module.parent) {
+    module.exports = app;
+  }
+
+  harp.compile(__dirname + '/public', outputPath, function (errors) {
     if (errors) {
       console.log(JSON.stringify(errors, null, 2));
       process.exit(1);
     }
 
-    console.log('Running harp-static on ' + port);
-    server(outputPath, port);
+    if (!module.parent) {
+      console.log('Running harp-static on ' + port);
+      var server = app.listen(port, function(){
+        console.log('Listening at http://%s:%s', server.address().address, server.address().port);
+      });
+    }
   });
 }
